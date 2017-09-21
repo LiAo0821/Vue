9cd3db2307d12b198d57a76d7c83a291625f4dde
Li ao <1224632854@qq.com>
2017-09-21 17:47:03 +0800

    起步

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <mt-header fixed title="广西高校思政在线"></mt-header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped>
+.mint-header{
+  color: #b71c1c;
+  background-color: #fff;
+  font-size: 1.03em;
+  font-weight: bold;
+}
+</style>
